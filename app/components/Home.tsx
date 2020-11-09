fe258dd1ee91fa1954628d72de3aaf4065e13ff4
leonyh77 <912661484@qq.com>
2020-11-09 19:04:37 +0800

    1.4.2

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React from 'react';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
+      <h2>Home222</h2>
       <Link to={routes.COUNTER}>to Counter</Link>
     </div>
   );
